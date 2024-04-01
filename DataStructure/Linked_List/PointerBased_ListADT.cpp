f34cbc29b558820765b5e589a5c4d3570e4758fc
bnhan2710 <baonhannice@gmail.com>
2024-04-01 19:36:42 +0700

    linkedlist

---
+++ DataStructure/Linked_List/PointerBased_ListADT.cpp
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct ElementType {
+    unsigned ID;
+    char *name;
+};
+
+struct Node {
+    struct ElementType value;
+    struct Node *next;
+};
+
+typedef struct Node *List;
+typedef struct Node *Position;
+
+Position Insert(List *pL , struct ElementType e, Position p){
+    Position newItem;
+    newItem = (Position)malloc(sizeof(struct Node));
+    newItem->value = e;
+    if (*pL == NULL || p == NULL){
+        newItem->next = *pL;
+        *pL = newItem;
+    }
+    else {
+        newItem->next = p->next;
+        p->next = newItem;
+    }
+    return newItem;
+}
+
+Position PushBack(List *pL, struct ElementType e){
+    Position newItem;
+    newItem = (Position)malloc(sizeof(struct Node));
+    newItem->value = e;
+    newItem->next = NULL;
+    if (*pL == NULL){
+        *pL = newItem;
+    }
+    else {
+        Position p;
+        p = *pL;
+        while (p->next != NULL){
+            p = p->next;
+        }
+        p->next = newItem;
+    }
+    return newItem;
+}
+
+void Display(List pL){
+    int count = 1;
+    while (pL != NULL)
+    {
+        printf("***%d        %d | %s \n",count, pL->value.ID, pL->value.name);
+    printf("\n");
+    pL = pL->next;
+    count++;
+    }
+}
+ 
+int main(){
+    List pL = NULL;
+    struct ElementType e1,e2,e3;
+    e1.ID = 102230034;
+    e1.name = "Huynh Nhu Bao Nhan ";
+    e2.ID = 102230008;
+    e2.name = "Nguyen Tan Dat";
+    e3.ID = 102230007;
+    e3.name = "Vo Van Cuong";
+    Position p;
+    p = Insert(&pL, e1, p);
+    p = Insert(&pL, e2, p);
+    p = Insert(&pL, e3, p);
+    Display(pL);
+    PushBack(&pL, e1);
+    Display(pL);
+    return 0;
+} 
---
+++ DataStructure/Linked_List/PointerBased_ListADT_header.cpp
@@ -0,0 +1,76 @@
+#include <stdio.h>
+
+struct ElementType {
+	unsigned long ID;
+	char *name;
+};
+
+struct Node {
+	struct ElementType value;
+	struct Node *next;
+};
+
+typedef struct Node *List;
+typedef struct Node *Position;
+
+Position insert(struct ElementType e, Position p){	
+	Position newItem;
+	newItem = malloc(sizeof(struct Node));
+	newItem->value = e;
+	
+	newItem->next = p->next;
+	p->next = newItem;
+	
+	return newItem;
+}
+
+void display(List pL)
+{
+	int count = 1;
+	pL = pL->next;
+	while (pL!=NULL)
+	{
+		printf("*** %d | %ld       | %s\n",count,pL->value.ID, pL->value.name);
+		pL = pL->next;
+		count++;
+	}
+}
+
+Position find(List pL, unsigned long ID) {
+	pL=pL->next;
+	while (pL!=NULL && pL->value.ID!=ID)
+		pL = pL->next;
+	return pL;
+}
+
+List createList() {
+	List headerNode = malloc(sizeof(struct Node));
+	headerNode->next = NULL;
+	return headerNode;
+}
+
+int main()
+{
+	List pL = createList();
+	struct ElementType e1,e2,e3;
+	
+	e1.ID = 123; e1.name = "Nguyen Van A";
+	e2.ID = 124; e2.name = "Dang Thi B";
+	e3.ID = 125; e3.name = "Do Van C";
+	
+	Position p = NULL;
+	
+	p = insert(e1,pL);
+	p = insert(e2,p);
+	p = insert(e3,p);
+	
+	display(pL);
+		
+	p = find(pL,124);
+	
+	if (p!=NULL) 
+		printf("Found!!!  ID: %ld         Name:%s\n",p->value.ID,p->value.name);
+	else
+		printf("Not Found!!!");
+	return 0;
+}
